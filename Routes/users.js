f292912d0d4e3260cec16cbcb58f0a1019509731
ale182 <alestalves@gmail.com>
2019-06-29 02:07:55 -0300

    Aulas 4 - 5. Criando schemas do mongoose

--- Routes/users.js
+++ Routes/users.js
@@ -1,17 +1,39 @@
 const express = require('express') ;
 const router = express.Router();
+const Users = require('../model/user');
+
 
 router.get('/',(req , res)=> {
-    return res.send({message: "OK GET da Rota Usuarios"});
-})
-
-router.post('/',(req , res)=> {
-    return res.send({message: "OK POST da Rota Usuarios"});
-})
-
+    Users.find({}, (err,data) => {
+        if (err) return res.send({error: 'Erro consulta de usuarios'});
+        return res.send(data);
+         });
+    });
 
 router.post('/create' , (req , res) => {
-    return res.send({message:'Usuario criado'})
+    /* Jeito antigo 
+    const obj = req.body ;
+    if(!obj.email || !obj.password) return res.send({ error: 'Dados insuficientes'});
+    */
+
+    // Agora desestruturado
+    const {email , password } = req.body ;
+
+    if(!email || !password) return res.send({ error: 'Dados insuficientes'});
+
+    Users.findOne({email}, (err , data) => {
+        if (err) return res.send({error: 'Erro ao buscar usuario'});
+        if (data) return res.send({error: 'Usuario ja criado'});
+
+        Users.create(req.body , (err,data) => {
+        // ou
+        // Users.create({email,password})
+
+            if (err) return res.send({error: 'Erro ao criar usuario'});
+            data.password = undefined ;
+            return res.send(data);
+        } )
+    })
 })
 
 module.exports = router ;
