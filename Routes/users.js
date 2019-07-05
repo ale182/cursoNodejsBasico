d99f824e8ee6b2d5366ec94923af70ec32ec174e
ale182 <alestalves@gmail.com>
2019-07-05 01:02:03 -0300

    Aulas 7 - Async e Await

--- Routes/users.js
+++ Routes/users.js
@@ -3,18 +3,34 @@ const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
 
+
+/* funcao antiga, antes de colocar aSync - erro de CallBack Hell
 router.get('/',(req , res)=> {
     Users.find({}, (err,data) => {
         if (err) return res.send({error: 'Erro consulta de usuarios'});
         return res.send(data);
          });
     });
+*/
 
+// Funcao com async
+router.get('/', async(req , res)=> {
+    try{
+        const users = await Users.find({});
+        return res.send(users);
+    } catch (err){
+        return res.send({error: 'Erro consulta de usuarios'});
+    }
+});
+
+
+/*
+/* funcao antiga, antes de colocar aSync - erro de CallBack Hell
 router.post('/create' , (req , res) => {
     /* Jeito antigo 
     const obj = req.body ;
     if(!obj.email || !obj.password) return res.send({ error: 'Dados insuficientes'});
-    */
+    /
 
     // Agora desestruturado
     const {email , password } = req.body ;
@@ -34,8 +50,28 @@ router.post('/create' , (req , res) => {
             return res.send(data);
         } )
     })
-})
+}) */
 
+// Funcao com async
+router.post('/create' ,  async (req , res) => {
+    const {email , password } = req.body ;
+
+    if(!email || !password) return res.send({ error: 'Dados insuficientes'});
+
+    try {
+        if (await Users.findOne({email})) 
+            return res.send({error: 'Usuario ja criado'});
+
+        const user = await Users.create(req.body);
+        user.password = undefined ;
+        return res.send(user);
+    } 
+    catch (err) {
+        return res.send({error: 'Erro ao buscar usuario'});
+    }
+});
+
+/* funcao antiga, antes de colocar aSync - erro de CallBack Hell
 router.post('/auth' , (req,res) => {
     const { email , password} = req.body ;
 
@@ -53,5 +89,28 @@ router.post('/auth' , (req,res) => {
         })
     }).select('+password');
 })
+*/
+
+router.post('/auth' , async (req,res) => {
+    const { email , password} = req.body ;
+
+    if (!email || !password) return res.send({ error: 'Dados insuficientes'})
+
+    try {
+        const user = await Users.findOne({email}).select('+password');
+        if(!user) 
+          return res.send({ error: 'Usuario nao registrado'});
+
+        const pass_ok = await bcrypt.compare(password ,user.password);
+        if(!pass_ok) return res.send({error: 'Erro ao autenticar usuario'}) ;
+
+        user.password = undefined ;
+        return res.send(user);
+    } 
+    catch (err) {
+        return res.send({ error: 'Erro ao buscar usuario'}) ;
+    }
+
+});
 
 module.exports = router ;
