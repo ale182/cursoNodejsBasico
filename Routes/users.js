07fc632d12eb6265973b98a12aedfb4fb59854b2
ale182 <alestalves@gmail.com>
2019-07-05 00:30:11 -0300

    Aula 6 - Criptografia com o bcrypt

--- Routes/users.js
+++ Routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express') ;
 const router = express.Router();
 const Users = require('../model/user');
-
+const bcrypt = require('bcrypt');
 
 router.get('/',(req , res)=> {
     Users.find({}, (err,data) => {
@@ -34,6 +34,24 @@ router.post('/create' , (req , res) => {
             return res.send(data);
         } )
     })
+})
+
+router.post('/auth' , (req,res) => {
+    const { email , password} = req.body ;
+
+    if (!email || !password) return res.send({ error: 'Dados insuficientes'})
+
+    Users.findOne({ email} , (err,data) => {
+        if(err) return res.send({ error: 'Erro ao buscar usuario'}) ;
+        if(!data) return res.send({ error: 'Usuario nao registrado'});
+
+        bcrypt.compare(password ,data.password , (err , same) => {
+            if(!same) return res.send({error: 'Erro ao autenticar usuario'}) ;
+
+            data.password = undefined ;
+            return res.send(data);
+        })
+    }).select('+password');
 })
 
 module.exports = router ;
